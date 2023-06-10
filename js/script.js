7bb3bb88dcaf79a157d56e828fa91a2898ca6d93
Nehuen Franco <teonehuen409@gmail.com>
2023-06-10 11:20:34 -0300

    DOM

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
-let horarioFinal;
-let dia;
-let diaDisponible;
 let profesionalTrabajando;
-let nombrePaciente;
-let dniPaciente;
-let apellidoPaciente;
-let keyFinal;
-let nombreProfesional;
 const profesionales = [
     "juan",
     "maria",
@@ -20,105 +12,205 @@ const diasSemana = [
     "viernes"
 ]
 class turnosReservados {
-    constructor(dia, horario, horarioFinal, nombreProfesional, nombrePaciente, apellidoPaciente, dniPaciente, confirmado) {
+    constructor(nombrePacienteYApellido, dniPaciente, dia, horario, nombreProfesional) {
+        this.nombrePaciente = nombrePacienteYApellido;
+        this.dniPaciente = dniPaciente;
         this.dia = dia;
         this.horario = horario;
-        this.horarioFinal = horarioFinal;
         this.nombreProfesional = nombreProfesional;
-        this.nombrePaciente = nombrePaciente;
-        this.apellidoPaciente = apellidoPaciente;
-        this.dniPaciente = dniPaciente;
-        this.confirmado = confirmado;
     }
 }
 const turnos = []
 
-class pacienteDatos {
-    constructor(nombre, apellido, dni) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.dni = dni;
-    }
-}
-
 function validarProfesional (nombreProfesional) {
     if (profesionales.includes(nombreProfesional)) {
-        alert(`Medico/a ${nombreProfesional} trabaja en este establecimiento`);
-        profesionalTrabajando = true;
-        return nombreProfesional;
+        return true;
     } else {
-        alert(`Medico/a ${nombreProfesional} no trabaja en este establecimiento`);
-        profesionalTrabajando = false;
+        return false;
     }
 }
 
-function turnosAmPm (horario) {
-    if (horario == "am") {
-        dia = prompt("Qué día desea reservar?").toLowerCase();
-        diaDisponible = diaAM.indexOf(dia);
-        if (diaDisponible >= 0) {
-            horarioFinal = prompt("Los horarios disponibles son: 10:00, 10:30, 11:00, 11:30, 12:00. Cuando desea reservar?");
-            keyFinal = true;
-            return horario = horario;
+//creador de cartas con turnos
+function creadorTarjetasTurnos(turno, index) {
+    const vitrina = document.getElementById("vitrinaTurnos")
+    const divTurnos = document.createElement("div");
+
+    const paciente = document.createElement("h3");
+    paciente.textContent = `Paciente: ${turno.nombrePaciente}`;
+
+    const dni = document.createElement("h4");
+    dni.textContent = `Dni: ${turno.dniPaciente}`;
+
+    const dia = document.createElement("h3");
+    dia.textContent = `Día: ${turno.dia}`;
+
+    const horario = document.createElement("h4");
+        horario.textContent = `Horario: ${turno.horario}`;
+
+    const profesional = document.createElement("h3");
+    profesional.textContent = `Profesional: ${turno.nombreProfesional}`;
+
+    const cancelarbttn = document.createElement("button");
+    cancelarbttn.innerText = "Cancelar turno";
+    cancelarbttn.addEventListener("click", () => {
+        let index = turnos.indexOf(turno);
+        if(index > -1){
+            turnos.splice(index, 1);
+            divTurnos.remove();
+            LSAcutalizado();
+        }
+    });
+
+    divTurnos.appendChild(paciente);
+    divTurnos.appendChild(dni);
+    divTurnos.appendChild(dia);
+    divTurnos.appendChild(horario);
+    divTurnos.appendChild(profesional);
+    divTurnos.appendChild(cancelarbttn);
+    vitrina.appendChild(divTurnos);
+}
+
+
+
+//validaDatos
+function ValidarDatos(name, pass){
+    let autenticado;
+    if(name == "Carlos" && pass == "TerceraPreentrega"){
+        return autenticado = true;
+    } else {
+        return autenticado = false
+    }
+}
+
+//login
+function Login() {
+    let login = document.getElementById("login");
+    const formLogin = document.createElement("form");
+
+    const loginNombre = document.createElement("input");
+    loginNombre.setAttribute("type", "text");
+    loginNombre.setAttribute("name", "nombre");
+    loginNombre.setAttribute("placeholder", "Ingrese su nombre");
+    formLogin.appendChild(loginNombre);
+
+    const loginClave = document.createElement("input");
+    loginClave.setAttribute("type", "password");
+    loginClave.setAttribute("name", "clave");
+    loginClave.setAttribute("placeholder", "Ingrese su clave");
+    formLogin.appendChild(loginClave);
+
+    const loginSubmit = document.createElement("button");
+    loginSubmit.setAttribute("type", "submit");
+    loginSubmit.innerText = "Enviar";
+    formLogin.appendChild(loginSubmit);
+    formLogin.addEventListener("submit", (e) => {
+        e.preventDefault();
+        
+        let name = loginNombre.value;
+        let pass = loginClave.value;
+        let autenticado = ValidarDatos(name, pass);
+        
+        if (autenticado) {
+            localStorage.setItem("secretarioLogueadoLS", autenticado);
+            login.style.display = "none";
+            IniciaAgenda();
+            ayudalogin.style.display = "none";
+            profesionalesAyuda.style.display = "block";
+        }  else {
+            formLogin.reset();
+        }
+    })
+    login.appendChild(formLogin);
+}
+
+//vitrina crea turnos
+function IniciaAgenda(){
+    let agenda = document.getElementById("agendaTurnos");
+    const formAgenda = document.createElement("form");
+
+    const nombrePaciente = document.createElement("input");
+    nombrePaciente.setAttribute("type", "text");
+    nombrePaciente.setAttribute("name", "nombre");
+    nombrePaciente.setAttribute("placeholder", "Nombre y apellido paciente");
+    nombrePaciente.setAttribute("required", "")
+    formAgenda.appendChild(nombrePaciente);
+
+    const dni = document.createElement("input");
+    dni.setAttribute("type", "text");
+    dni.setAttribute("name", "nombre");
+    dni.setAttribute("placeholder", "Dni paciente");
+    dni.setAttribute("required", "");
+    formAgenda.appendChild(dni);
+
+    const dia = document.createElement("input");
+    dia.setAttribute("type", "text");
+    dia.setAttribute("name", "nombre");
+    dia.setAttribute("placeholder", "Día del turno");
+    dia.setAttribute("required", "");
+    formAgenda.appendChild(dia);
+
+    const horario = document.createElement("input");
+    horario.setAttribute("type", "text");
+    horario.setAttribute("name", "nombre");
+    horario.setAttribute("placeholder", "Horario del turno");
+    horario.setAttribute("required", "");
+    formAgenda.appendChild(horario);
+
+    const profesional = document.createElement("input");
+    profesional.setAttribute("type", "text");
+    profesional.setAttribute("name", "nombre");
+    profesional.setAttribute("placeholder", "Profesional");
+    profesional.setAttribute("required", "");
+    formAgenda.appendChild(profesional);
+
+    const submitAgenda = document.createElement("button");
+    submitAgenda.setAttribute("type", "submit");
+    submitAgenda.innerText = "Crear";
+    formAgenda.appendChild(submitAgenda);
+    formAgenda.addEventListener("submit", (e) => {
+        e.preventDefault();
+        if(validarProfesional(profesional.value)){
+            const turnoNuevo = new turnosReservados(nombrePaciente.value, dni.value, dia.value, horario.value, profesional.value);
+            turnos.push(turnoNuevo);
+            creadorTarjetasTurnos(turnoNuevo);
+            LSAcutalizado();
+            formAgenda.reset();
         } else {
-            alert(`No se puede reservar para el día ${dia} a la mañana`);
-            keyFinal = false;
+            alert("Profesional no coincidide con la base de datos");
         }
-    } else if(horario == "pm") {
-        dia = prompt("Qué día desea reservar?").toLowerCase();
-        diaDisponible = diaPM.indexOf(dia);
-        if (diaDisponible >= 0) {
-            horarioFinal = prompt("Los horarios disponibles son: 14:00, 14:30, 15:00, 15:30, 16:00, 16:30, 17:00, 17:30, 18:00. Cuando desea reservar?");
-            keyFinal = true;
-            return horario = horario;
-        } else {
-            alert(`No se puede reservar para el día ${dia} a la tarde`);
-            keyFinal = false;
-        }
-    } else {
-        alert("Error: ud no ha seleccionado ni Am, ni Pm");
-        keyFinal = false;
+    })
+    agenda.appendChild(formAgenda);
+}
+
+//pasar turnos a json
+function LSAcutalizado() {
+    let turnosActuales = JSON.stringify(turnos);
+    localStorage.setItem("TurnosLS", turnosActuales);
+}
+
+function recargaTurnos(){
+    let turnosLS = localStorage.getItem("TurnosLS");
+    if(turnosLS != undefined){
+        turnos.splice(0, turnos.length);
+        turnosLS = JSON.parse(turnosLS);
+        turnos.push(...turnosLS);
+        turnos.forEach((turno, index) => { creadorTarjetasTurnos(turno, index)});
     }
 }
 
-function confirmarDatos () {
-    nombrePaciente = prompt("Ingrese su nombre");
-    apellidoPaciente = prompt("Ingrese su apellido");
-    dniPaciente = prompt("Ingrese su número de documento");
-    alert(`Datos confirmados, ${nombrePaciente} ${apellidoPaciente}, con el dni ${dniPaciente}`);
-}
+//log in de secretaria
+const secretarioLogueadoLS = localStorage.getItem('secretarioLogueadoLS');
+let ayudalogin = document.getElementById("AyudaLogin");
+let profesionalesAyuda = document.getElementById("Profesionales");
+profesionalesAyuda.style.display = "none";
 
-function confirmaciónTurno () {
-    if (profesionalTrabajando == true) {
-        horario = turnosAmPm(prompt("Desea reservar: am o pm?").toLowerCase());
-        if (keyFinal == true) {
-            alert(`Su turno con ${nombreProfesional} ha sido reservado a nombre de ${nombrePaciente}, dni:${dniPaciente}, para el día ${dia} a las ${horarioFinal} ${horario}.`);
-        }
-    } else {
-        alert(`Error: Datos invalidos o no correspondientes. No se pudo reservar un turno`);
-    }
-}
-
-//*Codigo para separar entre los días de la mañana y la tarde
-const diaAM = diasSemana.filter((elemento, indice) => indice % 2 === 0);
-const diaPM = diasSemana.filter((elemento, indice) => indice % 2 !== 0);
-
-
-//*Empieza el proceso del sistema de turnos
-alert("Bienvenido al sistema de reservas para turnos de odontología");
-confirmarDatos();
-const paciente = new pacienteDatos(nombrePaciente, apellidoPaciente, dniPaciente);
-let turnosReservar = prompt("Cuantos turnos desea reservar?");
-console.log(turnosReservar);
-if (isNaN(turnosReservar)) {
-    alert(`${turnosReservar} no es un número`);
+if(secretarioLogueadoLS){
+    console.log("logueado")
+    ayudalogin.style.display = "none";
+    IniciaAgenda();
+    profesionalesAyuda.style.display = "block";
 } else {
-    alert("Nuestros profesionales son: juan, maria y roberto.");
-    for (i = 0; i < turnosReservar; i++){
-        nombreProfesional = validarProfesional(prompt("Indique el nombre del medico para el turno").toLowerCase());
-        confirmaciónTurno();
-        const turnoNuevo = new turnosReservados(dia, horario, horarioFinal, nombreProfesional, nombrePaciente, apellidoPaciente, dniPaciente, keyFinal);
-        turnos.push(turnoNuevo);
-    }
+    Login();
 }
-console.table(turnos);
+
+recargaTurnos();
